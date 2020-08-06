831e8e75674735445673c6f741f7f1e3fda292e9
iinfin <han@iinf.in>
2020-08-06 17:14:22 +0300

    edit

--- static/worker.js
+++ static/worker.js
@@ -1,61 +1,69 @@
 "use strict";
 
-var version = "v1::",
-	offlineFundamentals = ["", "/"];
+var version = "v1::";
 
-self.addEventListener("install", function (e) {
-	e.waitUntil(
+var offlineFundamentals = ["", "/"];
+
+self.addEventListener("install", function (event) {
+	event.waitUntil(
 		caches
 			.open(version + "fundamentals")
-			.then(function (e) {
-				return e.addAll(offlineFundamentals);
+			.then(function (cache) {
+				return cache.addAll(offlineFundamentals);
 			})
 			.then(function () {})
 	);
 });
 
-self.addEventListener("fetch", function (e) {
-	"GET" === e.request.method &&
-		e.respondWith(
-			caches.match(e.request).then(function (n) {
-				var t = fetch(e.request)
-					.then(function (n) {
-						var t = n.clone();
-						return (
-							caches
-								.open(version + "pages")
-								.then(function (n) {
-									return n.put(e.request, t);
-								})
-								.then(function () {}),
-							n
-						);
-					}, a)
-					.catch(a);
-				return n || t;
-				function a() {
-					return new Response("<h1>Service Unavailable</h1>", {
-						status: 503,
-						statusText: "Service Unavailable",
-						headers: new Headers({ "Content-Type": "text/html" }),
-					});
-				}
-			})
-		);
+self.addEventListener("fetch", function (event) {
+	if (event.request.method !== "GET") {
+		return;
+	}
+	event.respondWith(
+		caches.match(event.request).then(function (cached) {
+			var networked = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
+
+			return cached || networked;
+
+			function fetchedFromNetwork(response) {
+				var cacheCopy = response.clone();
+
+				caches
+					.open(version + "pages")
+					.then(function add(cache) {
+						return cache.put(event.request, cacheCopy);
+					})
+					.then(function () {});
+
+				return response;
+			}
+
+			function unableToResolve() {
+				return new Response("<h1>Service Unavailable</h1>", {
+					status: 503,
+					statusText: "Service Unavailable",
+					headers: new Headers({
+						"Content-Type": "text/html",
+					}),
+				});
+			}
+		})
+	);
 });
 
-self.addEventListener("activate", function (e) {
-	e.waitUntil(
+self.addEventListener("activate", function (event) {
+	event.waitUntil(
 		caches
+
 			.keys()
-			.then(function (e) {
+			.then(function (keys) {
 				return Promise.all(
-					e
-						.filter(function (e) {
-							return !e.startsWith(version);
+					keys
+						.filter(function (key) {
+							return !key.startsWith(version);
 						})
-						.map(function (e) {
-							return caches.delete(e);
+						.map(function (key) {
+							return caches.delete(key);
 						})
 				);
 			})
