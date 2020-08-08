f3fc3744a0605a9012b0cfde9ab8296c0eed2cfd
iinfin <han@iinf.in>
2020-08-08 22:42:01 +0300

    edit worker

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,18 +1,9 @@
-window.onload = () => {
-	if ("serviceWorker" in navigator) {
+"serviceWorker" in navigator &&
+	window.addEventListener("load", () => {
 		navigator.serviceWorker
 			.register("../worker.js")
-			.then(function (reg) {
-				if (reg.installing) {
-					// console.log("[Service worker] installing");
-				} else if (reg.waiting) {
-					// console.log("[Service worker] installed");
-				} else if (reg.active) {
-					// console.log("[Service worker] active");
-				}
-			})
-			.catch(function (error) {
-				// console.log("[Service worker] failed with " + error);
+			.then((r) => {})
+			.catch((r) => {
+				console.error("[Service Worker]", r);
 			});
-	}
-};
+	});
